c59fe0ec7d5c0d301a0497b79b011e9185e9a24e
David Tiago Conceição <davidtc.developer@gmail.com>
2017-10-17 09:02:05 -0200

    Removing rx stuff from presenter

--- android-moviedb/app/src/main/java/com/davidtiagoconceicao/androidmovies/mvp/list/UpcomingListPresenter.java
+++ android-moviedb/app/src/main/java/com/davidtiagoconceicao/androidmovies/mvp/list/UpcomingListPresenter.java
@@ -3,21 +3,11 @@ package com.davidtiagoconceicao.androidmovies.mvp.list;
 import android.util.Log;
 import android.util.LongSparseArray;
 
-import com.davidtiagoconceicao.androidmovies.commons.MovieUtil;
 import com.davidtiagoconceicao.androidmovies.data.Genre;
 import com.davidtiagoconceicao.androidmovies.data.ImageConfiguration;
-import com.davidtiagoconceicao.androidmovies.data.Movie;
 import com.davidtiagoconceicao.androidmovies.data.remote.configuration.ConfigurationRepository;
 import com.davidtiagoconceicao.androidmovies.data.remote.genre.GenresRemoteRepository;
 import com.davidtiagoconceicao.androidmovies.data.remote.movie.MoviesRemoteRepository;
-
-import java.util.List;
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Function;
-import io.reactivex.observers.ResourceSingleObserver;
-import io.reactivex.subscribers.ResourceSubscriber;
 
 /**
  * Presenter for upcoming movies list.
@@ -27,7 +17,6 @@ import io.reactivex.subscribers.ResourceSubscriber;
 
 final class UpcomingListPresenter implements UpcomingListContract.Presenter {
 
-    private final CompositeDisposable compositeDisposable;
     private final UpcomingListContract.View view;
     private final MoviesRemoteRepository moviesRemoteRepository;
     private final GenresRemoteRepository genresRemoteRepository;
@@ -48,7 +37,6 @@ final class UpcomingListPresenter implements UpcomingListContract.Presenter {
         this.moviesRemoteRepository = moviesRemoteRepository;
         this.genresRemoteRepository = genresRemoteRepository;
         this.configurationRepository = configurationRepository;
-        this.compositeDisposable = new CompositeDisposable();
 
         this.view.setPresenter(this);
     }
@@ -70,12 +58,12 @@ final class UpcomingListPresenter implements UpcomingListContract.Presenter {
 
     @Override
     public void onDetach() {
-        compositeDisposable.clear();
+
     }
 
     @Override
     public void onDestroy() {
-        compositeDisposable.dispose();
+
     }
 
     @Override
@@ -102,88 +90,16 @@ final class UpcomingListPresenter implements UpcomingListContract.Presenter {
     @SuppressWarnings("WeakerAccess")
     void loadMoreItems() {
         view.showLoading(true);
-        compositeDisposable.add(
-                moviesRemoteRepository.getUpcoming(currentPageCount)
-                        .map(new Function<Movie, Movie>() {
-                            @Override
-                            public Movie apply(Movie movie) throws Exception {
-                                return MovieUtil.mapMovieFields(movie, imageConfiguration, genres);
-                            }
-                        })
-                        .toList()
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribeWith(new ResourceSingleObserver<List<Movie>>() {
-                            @Override
-                            public void onSuccess(List<Movie> movies) {
-
-                                if (currentPageCount == 1) {
-                                    view.clearList();
-                                }
-                                view.addMovies(movies);
-
-                                view.showLoading(false);
-                            }
-
-                            @Override
-                            public void onError(Throwable throwable) {
-
-                                view.showLoading(false);
-                            }
-                        }));
     }
 
     //Called by inner classes
     @SuppressWarnings("WeakerAccess")
     void loadGenres() {
-        compositeDisposable.add(
-                genresRemoteRepository.getGenres()
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribeWith(new ResourceSubscriber<Genre>() {
-                            @Override
-                            public void onNext(Genre genre) {
-                                genres.put(genre.id(), genre);
-                            }
-
-                            @Override
-                            public void onError(Throwable t) {
-                                handleException(t);
-                            }
-
-                            @Override
-                            public void onComplete() {
-                                if (imageConfiguration == null) {
-                                    loadImageConfiguration();
-                                } else {
-                                    refresh();
-                                }
-                            }
-                        }));
     }
 
     //Called by inner classes, default avoid accessors
     @SuppressWarnings("WeakerAccess")
     void loadImageConfiguration() {
-        compositeDisposable.add(
-                configurationRepository.getImageConfiguration()
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .toFlowable()
-                        .subscribeWith(
-                                new ResourceSubscriber<ImageConfiguration>() {
-                                    @Override
-                                    public void onNext(ImageConfiguration imageConfigurationResponse) {
-                                        imageConfiguration = imageConfigurationResponse;
-                                    }
-
-                                    @Override
-                                    public void onError(Throwable t) {
-                                        handleException(t);
-                                    }
-
-                                    @Override
-                                    public void onComplete() {
-                                        refresh();
-                                    }
-                                }));
     }
 
 }
--- android-moviedb/app/src/main/java/com/davidtiagoconceicao/androidmovies/mvp/search/SearchPresenter.java
+++ android-moviedb/app/src/main/java/com/davidtiagoconceicao/androidmovies/mvp/search/SearchPresenter.java
@@ -1,27 +1,10 @@
 package com.davidtiagoconceicao.androidmovies.mvp.search;
 
 import android.util.Log;
-import android.util.LongSparseArray;
 
-import com.davidtiagoconceicao.androidmovies.commons.MovieUtil;
-import com.davidtiagoconceicao.androidmovies.data.Genre;
-import com.davidtiagoconceicao.androidmovies.data.ImageConfiguration;
-import com.davidtiagoconceicao.androidmovies.data.Movie;
 import com.davidtiagoconceicao.androidmovies.data.remote.configuration.ConfigurationRepository;
 import com.davidtiagoconceicao.androidmovies.data.remote.genre.GenresRemoteRepository;
 import com.davidtiagoconceicao.androidmovies.data.remote.movie.MoviesRemoteRepository;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import io.reactivex.BackpressureStrategy;
-import io.reactivex.ObservableSource;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Function;
-import io.reactivex.functions.Predicate;
-import io.reactivex.subjects.PublishSubject;
-import io.reactivex.subscribers.ResourceSubscriber;
 
 /**
  * Presenter for upcoming movies list.
@@ -31,15 +14,10 @@ import io.reactivex.subscribers.ResourceSubscriber;
 
 final class SearchPresenter implements SearchContract.Presenter {
 
-    private final CompositeDisposable compositeDisposable;
     private final SearchContract.View view;
     private final MoviesRemoteRepository moviesRemoteRepository;
     private final GenresRemoteRepository genresRemoteRepository;
     private final ConfigurationRepository configurationRepository;
-
-    private LongSparseArray<Genre> genres;
-    private ImageConfiguration imageConfiguration;
-    private final PublishSubject<String> searchSubject;
 
     SearchPresenter(
             SearchContract.View view,
@@ -51,54 +29,7 @@ final class SearchPresenter implements SearchContract.Presenter {
         this.moviesRemoteRepository = moviesRemoteRepository;
         this.genresRemoteRepository = genresRemoteRepository;
         this.configurationRepository = configurationRepository;
-        this.compositeDisposable = new CompositeDisposable();
 
-        searchSubject = PublishSubject.create();
-        compositeDisposable.add(
-                searchSubject
-                        .filter(new Predicate<String>() {
-                            @Override
-                            public boolean test(String s) throws Exception {
-                                return s.length() >= 3;
-                            }
-                        })
-                        .debounce(300, TimeUnit.MILLISECONDS)
-                        .flatMap(new Function<String, ObservableSource<List<Movie>>>() {
-                            @Override
-                            public ObservableSource<List<Movie>> apply(String s) throws Exception {
-                                return SearchPresenter.this.moviesRemoteRepository.searchMovie(s)
-                                        .map(new Function<Movie, Movie>() {
-                                            @Override
-                                            public Movie apply(Movie movie) throws Exception {
-                                                return MovieUtil.mapMovieFields(movie, imageConfiguration, genres);
-                                            }
-                                        })
-                                        .toList()
-                                        .toObservable();
-                            }
-                        })
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .toFlowable(BackpressureStrategy.BUFFER)
-                        .subscribeWith(new ResourceSubscriber<List<Movie>>() {
-                            @Override
-                            public void onNext(List<Movie> movies) {
-                                SearchPresenter.this.view.showResult(movies);
-                                SearchPresenter.this.view.showLoading(false);
-                            }
-
-                            @Override
-                            public void onError(Throwable t) {
-                                SearchPresenter.this.handleException(t);
-                                SearchPresenter.this.view.showLoading(false);
-                            }
-
-                            @Override
-                            public void onComplete() {
-                                //Never completes
-                            }
-                        }));
-
-        this.view.setPresenter(this);
     }
 
     @Override
@@ -109,26 +40,15 @@ final class SearchPresenter implements SearchContract.Presenter {
 
     @Override
     public void onDetach() {
-        compositeDisposable.clear();
+
     }
 
     @Override
     public void onDestroy() {
-        compositeDisposable.dispose();
     }
 
     @Override
     public void search(final String query) {
-        if (genres == null) {
-            view.showLoading(true);
-
-            genres = new LongSparseArray<>();
-
-            loadGenres(query);
-
-        } else {
-            searchSubject.onNext(query);
-        }
     }
 
     //Called by inner classes, default avoid accessors
@@ -137,58 +57,5 @@ final class SearchPresenter implements SearchContract.Presenter {
         Log.e(getClass().getSimpleName(), "Exception on stream", e);
         view.showErrorLoading();
         view.showLoading(false);
-    }
-
-    //Called by inner classes, default avoid accessors
-    @SuppressWarnings("WeakerAccess")
-    void loadGenres(final String query) {
-        compositeDisposable.add(
-                genresRemoteRepository.getGenres()
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribeWith(new ResourceSubscriber<Genre>() {
-                            @Override
-                            public void onNext(Genre genre) {
-                                genres.put(genre.id(), genre);
-                            }
-
-                            @Override
-                            public void onError(Throwable t) {
-                                handleException(t);
-                            }
-
-                            @Override
-                            public void onComplete() {
-                                if (imageConfiguration == null) {
-                                    loadImageConfiguration(query);
-                                } else {
-                                    searchSubject.onNext(query);
-                                }
-                            }
-                        }));
-    }
-
-    //Called by inner classes, default avoid accessors
-    @SuppressWarnings("WeakerAccess")
-    void loadImageConfiguration(final String query) {
-        compositeDisposable.add(
-                configurationRepository.getImageConfiguration()
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .toFlowable()
-                        .subscribeWith(new ResourceSubscriber<ImageConfiguration>() {
-                            @Override
-                            public void onNext(ImageConfiguration imageConfigurationResponse) {
-                                imageConfiguration = imageConfigurationResponse;
-                            }
-
-                            @Override
-                            public void onError(Throwable t) {
-                                handleException(t);
-                            }
-
-                            @Override
-                            public void onComplete() {
-                                searchSubject.onNext(query);
-                            }
-                        }));
     }
 }
